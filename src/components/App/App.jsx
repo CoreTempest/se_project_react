68cc5009d8ca6167d4cb897a74dc0fe8a922c2e3
Ian <istuart20@gmail.com>
2024-07-29 17:21:12 -0700

    Main and WeatherCard set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
+import WeatherCard from "../WeatherCard/WeatherCard";
 
 function App() {
   return (
     <div className="app">
       <div className="app__content">
         <Header />
+        <Main />
       </div>
     </div>
   );
