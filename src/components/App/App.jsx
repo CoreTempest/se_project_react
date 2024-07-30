d62b2b79b0f446730a9454644f16bbc62e37f562
Ian <istuart20@gmail.com>
2024-07-29 17:46:25 -0700

    ItemCard basic renders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import WeatherCard from "../WeatherCard/WeatherCard";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
   return (
     <div className="app">
       <div className="app__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
